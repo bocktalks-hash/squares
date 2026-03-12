e502d45f19bc4845615937c566e2426a007ff7c2
bocktalks-hash <bocktalks@gmail.com>
2026-03-12 18:29:13 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,10 @@ import LandingPage from "./LandingPage";
 import PrivacyPolicy from "./PrivacyPolicy";
 import TutorialTour, { shouldShowTour } from "./TutorialTour";
 import { STORAGE_KEY, TO_STORAGE_KEY } from "./shared/constants";
-import { useAuth, useUser, SignInButton, SignOutButton, UserButton } from "@clerk/react";
+import { useAuth, useUser, SignInButton, SignOutButton, UserButton } from "@clerk/clerk-react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+
 
 const GUEST_KEY = "bt_guest_mode";
 
